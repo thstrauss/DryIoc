602661912d8b9ad08a98a78932192743017681ee
Maksim Volkau <max.volkov@gmail.com>
2017-12-16 20:50:06 +0300

    added: Test for SO question https://stackoverflow.com/questions/47813292/resolution-status-in-dryioc-container
    --HG--
    branch : v2

---
+++ Net45/DryIoc.IssuesTests/SO_IsInstantiatedViaDecorator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class SO_IsInstantiatedViaDecorator
+    {
+        [Test]
+        public void Test()
+        {
+            var c = new Container();
+            c.Register<IService, X>(Reuse.Singleton);
+
+            c.Register<XProvider>(Reuse.Singleton);
+
+            c.Register<IService>(
+                Made.Of(_ => ServiceInfo.Of<XProvider>(), p => p.Create(Arg.Of<Func<IService>>())),
+                Reuse.Singleton,
+                Setup.Decorator);
+
+            c.Register<A>();
+
+            var x = c.Resolve<XProvider>();
+            Assert.IsFalse(x.IsCreated);
+
+            c.Resolve<A>();
+
+            Assert.IsTrue(x.IsCreated);
+        }
+
+        public interface IService { }
+        public class X : IService { }
+
+        public class A
+        {
+            public A(IService service) { }
+        }
+
+        public class XProvider
+        {
+            public bool IsCreated { get; private set; }
+            public IService Create(Func<IService> factory)
+            {
+                IsCreated = true;
+                return factory();
+            }
+        }
+    }
+}
